b8c3128e92c4e1830f86174afd3485a577c9a441
goosle <yh.kim@goosle-mac.local>
2022-03-01 21:29:02 +0900

    Fix a bug on netlify

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,7 @@ import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		target: '#svelte'
+		adapter: adapter()
 	}
 };
 
